5f877c82c3f93c5779ba5faa86dd6f94102ef5b7
Enden,Luc L. van den <i453142@FontysICT.nl>
2024-11-17 09:59:25 +0100

    Add .gitignore and sjf_emulation.py (the file that is to contain the demo code)

---
+++ research_lvde/sjf_emulation.py
@@ -0,0 +1,6 @@
+def bow():
+    """
+    An atomic `body of work`. Meaning this function will either come to full completion once ran, or not ran at all.
+    """
+    print("Atomic bow started")
+    return "Atomic bow completed"
